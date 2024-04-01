e2a518fc0d77e1916b5cc449673e2d8f90466e0a
AbuTalha3 <najeebakhun@gmail.com>
2024-04-01 22:20:31 +0500

    Add for loop

---
+++ script.js
@@ -0,0 +1,17 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+  const strArray = str.split('');
+  const cleanStrArray = [];
+  for (let i = 0; i < strArray.length; i++){
+    if(!["+", "-", " "].includes(strArray[i])){
+      cleanStrArray.push(strArray[i])
+    }
+  }
+}
