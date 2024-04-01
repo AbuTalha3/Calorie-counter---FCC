3ba75b5ec258aaa65c5666ab95f2b7f9785d18aa
AbuTalha3 <najeebakhun@gmail.com>
2024-04-02 04:43:08 +0500

    Modify level 46

--- script.js
+++ script.js
@@ -18,5 +18,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
 }
